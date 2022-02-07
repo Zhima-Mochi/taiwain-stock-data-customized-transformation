8e419455a93c626e2c2f6d60aa129f5dc473475a
Zhima-Mochi <yehgoter@gmail.com>
2022-02-07 18:05:08 +0800

    update

--- data-transformation/data_transformation/utils.py
+++ data-transformation/data_transformation/utils.py
@@ -34,6 +34,25 @@ return_rate
 amihud_p
 amihud_m
 """
+"""
+ `data_date` date NOT NULL,
+    `stock_id` char(6) NOT NULL,
+    `matching_time` time(6) NOT NULL,
+    `is_matching` tinyint DEFAULT NULL,
+    `best_ask_tick_number` tinyint DEFAULT NULL,
+    `best_bid_tick_number` tinyint DEFAULT NULL,
+    `matching_price_limit_mark` tinyint DEFAULT NULL,
+    `best_ask_tick_price_limit_mark` tinyint DEFAULT NULL,
+    `best_bid_tick_price_limit_mark` tinyint DEFAULT NULL,
+    `momentary_price_movement` tinyint DEFAULT NULL,
+    `matching_price` float DEFAULT NULL,
+    `matching_volume` int DEFAULT NULL,
+    `the_best_ask_tick_price` float DEFAULT NULL,
+    `the_best_ask_tick_volume` int DEFAULT NULL,
+    `the_best_bid_tick_price` float DEFAULT NULL,
+    `the_best_bid_tick_volume` int DEFAULT NULL
+"""
+
 
 from bisect import bisect_left, bisect_right
 import asyncio
@@ -43,15 +62,14 @@ from data_transformation.crud import get_dates_in_month, get_each_date_content_d
 from data_transformation.models import create_sa_stock_data_table
 import pandas as pd
 import datetime
-
 stock_start_time = datetime.time(8, 30)
 stock_end_time = datetime.time(13, 30)
 
 
-start_seconds = stock_start_time.hour*60*60 + \
-    stock_start_time.minute*60+stock_start_time.second
-end_seconds = stock_end_time.hour*60*60 + \
-    stock_end_time.minute*60+stock_end_time.second
+start_mins = stock_start_time.hour*60 + \
+    stock_start_time.minute
+end_mins = stock_end_time.hour*60 + \
+    stock_end_time.minute
 
 
 def task(task_table_name):
@@ -60,9 +78,6 @@ def task(task_table_name):
         task_sa_table = create_sa_stock_data_table(task_table_name)
     except Exception as e:
         logging.warning(e)
-
-    periods = pd.DataFrame(
-        {'matching_time': (datetime.time(sec//60//60, sec % 3600//60, sec % 60) for sec in range(start_seconds, end_seconds+1, 1))})
 
     async def async_task():
         async with get_database() as database:
@@ -75,12 +90,56 @@ def task(task_table_name):
                     data = await get_each_date_content_dataframe(
                         database, task_sa_table, data_date)
                     stock_id_list = data["stock_id"].unique()
+                    # data conversion stock_id by stock_id in a date
                     for stock_id in stock_id_list:
                         stock_data = pd.DataFrame(data[bisect_left(data["stock_id"], stock_id):bisect_right(
                             data["stock_id"], stock_id)])
-                        stock_data = periods.join(stock_data.set_index(
-                            'matching_time'), on='matching_time')
+                        # get the nearest data per minute
+                        stock_data["matching_time"] = pd.to_datetime([datetime.datetime.combine(
+                            data_date, mt) for mt in stock_data["matching_time"]])
+                        stock_data["time"] = stock_data["matching_time"].dt.ceil(
+                            'T')
+                        # stock_data["buyer_side_init_match_count"]
+
+                        stock_data.loc[stock_data["the_best_bid_tick_price"].isnull(
+                        ), "the_best_bid_tick_price"] = stock_data["the_best_ask_tick_price"][stock_data["the_best_bid_tick_price"].isnull()]
+
+                        stock_data.loc[stock_data["the_best_bid_tick_price"].isnull(
+                        ), "the_best_bid_tick_price"] = stock_data["matching_price"][stock_data["the_best_bid_tick_price"].isnull()]
+
+                        stock_data.loc[stock_data["the_best_bid_tick_price"].isnull(
+                        ), "the_best_bid_tick_price"] = 0
+
+                        stock_data.loc[stock_data["the_best_ask_tick_price"].isnull(
+                        ), "the_best_ask_tick_price"] = stock_data["the_best_bid_tick_price"][stock_data["the_best_ask_tick_price"].isnull()]
+
+                        stock_data.loc[stock_data["the_best_ask_tick_price"].isnull(
+                        ), "the_best_ask_tick_price"] = stock_data["matching_price"][stock_data["the_best_ask_tick_price"].isnull()]
+
+                        stock_data["middle_price"] = (
+                            stock_data["the_best_bid_tick_price"]+stock_data["the_best_ask_tick_price"])/2
+
+                        stock_data["transaction_price"] = stock_data["matching_price"]
+                        stock_data.loc[stock_data["transaction_price"].isnull(
+                        ), "transaction_price"] = stock_data["middle_price"]
+
+                        stock_data = stock_data[stock_data["transaction_price"] > 0]
+
+                        stock_data["QBA"] = (
+                            stock_data["the_best_ask_tick_price"]-stock_data["the_best_bid_tick_price"])/stock_data["transaction_price"]
+
+                        stock_data["previous_transaction_price"] = stock_data["transaction_price"].shift(
+                            1)
+                            
+                        stock_data["indicator_q"] = [-1 if stock_data["transaction_price"][i] < stock_data["middle_price"][i]
+                                                     else 1 if stock_data["transaction_price"][i] > stock_data["middle_price"][i] else 1 if stock_data["transaction_price"][i] > stock_data["previous_transaction_price"][i] else -1 if stock_data["transaction_price"][i] < stock_data["previous_transaction_price"][i] else 0 for i in stock_data.index]
                         print(stock_data)
+                        # stock_data = stock_data.groupby("time").last()
+
+                        # stock_data = stock_data.reindex(pd.date_range(datetime.datetime.combine(
+                        #     data_date, stock_start_time), datetime.datetime.combine(data_date, stock_end_time), freq="T"))
+                        # stock_data = stock_data.ffill()
+
                 # set current table status finished
                 await set_task_table_record_status(database, task_table_name, "1")
             except Exception as e:
